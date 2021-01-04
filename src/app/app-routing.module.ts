16e482e6260f144712c16c4255f40422f4174bec
Юрій Штиба <shtybayu@gmail.com>
2021-01-04 22:10:14 +0200

    draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { UserGridConfiguration } from './user-grid/models/configurations/user-grid-configuration';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () => import('./user-grid/user-grid.module').then(m => m.UserGridModule.forRoot(
+      new UserGridConfiguration(
+        environment.apiUrl,
+        'api/player',
+        'api/someData'
+      )
+    )),
+    canActivate: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {UserGridModule} from "./user-grid/user-grid.module";
-import {UserGridConfiguration} from "./user-grid/models/configurations/user-grid-configuration";
+import { UserGridModule } from './user-grid/user-grid.module';
+import { UserGridConfiguration } from './user-grid/models/configurations/user-grid-configuration';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -14,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserGridModule.forRoot(new UserGridConfiguration('http://localhost:8080/api/player')),
     BrowserAnimationsModule,
   ],
   providers: [],
